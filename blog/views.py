0ee099f48cb5492ed9bb1ee1adb874856a8a3741
Josecarlos256 <josecarlosha@mesoamericana.edu.gt>
2021-09-07 22:54:49 -0600

    Vistas con formularios Django

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+    
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
